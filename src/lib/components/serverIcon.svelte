401104c27b292ed07c496487f789b6208f7fdabc
Heráclito <heraclitoqsaldanha@gmail.com>
2026-04-03 04:46:16 -0300

    melhora popup ao passar mouse por cima do icone de um servidor

--- src/lib/components/serverIcon.svelte
+++ src/lib/components/serverIcon.svelte
@@ -15,7 +15,7 @@
 >
     <img src={`${data.url}/icon`} alt="icon" class="h-12 w-12" />
     <div
-        class="absolute bg-slate-700 w-20 rounded-md items-center justify-center flex left-full ml-2 opacity-0 translate-x-2 transition-all duration-200 group-hover:opacity-100 group-hover:translate-x-0"
+        class="absolute bg-slate-700 p-1 rounded-md items-center justify-center flex left-full ml-2 opacity-0 translate-x-2 transition-all duration-200 group-hover:opacity-100 group-hover:translate-x-0 pointer-events-none"
     >
         <p>{data.title}</p>
     </div>
