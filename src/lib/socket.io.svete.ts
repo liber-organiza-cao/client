9a5dff59c01d3dc9ff7e5f281c794e98231aebdf
Heráclito <heraclitoqsaldanha@gmail.com>
2026-04-12 04:26:32 -0300

    atualiza interface ClientToServerEvents

--- src/lib/socket.io.svete.ts
+++ src/lib/socket.io.svete.ts
@@ -14,8 +14,12 @@ export interface ClientToServerEvents {
     joinChannel: (channelId: string, callback: (success: boolean) => void) => void,
     sendMessage: (message: string) => void,
     loadMessages: (beforeId?: string, callback?: (messages: Message[]) => void) => void,
+
+    createChannel: (name: string, callback: (success: boolean) => void) => void,
+    deleteChannel: (channelId: string, callback: (success: boolean) => void) => void,
+
     requestAuthChallenge: (publicKey: number[], callback: (secret: number[]) => void) => void,
-    confirmAuthChallenge: (signature: number[], callback: (valid: boolean) => void) => void,
+    confirmAuthChallenge: (signature: number[], callback: (success: boolean, isAdmin: boolean) => void) => void,
 }
 
 export type SocketConnection = Socket<ServerToClientEvents, ClientToServerEvents>;
