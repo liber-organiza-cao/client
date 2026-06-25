7472a7051ed4bbb223156c312fa091998788f79b
Heráclito <heraclitoqsaldanha@gmail.com>
2026-06-24 22:40:18 -0300

    implementa suporte para arquivos

---
+++ src/lib/components/filePreview.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import {
+        faFile,
+        faMusic,
+        faVideo,
+    } from "@fortawesome/free-solid-svg-icons";
+    import Fa from "svelte-fa";
+
+    const { file }: { file: File } = $props();
+</script>
+
+<div
+    class="flex items-center justify-center w-32 h-32 bg-gray-950/25 rounded-xl"
+>
+    {#if file.type.startsWith("image/")}
+        <div class="flex items-center justify-center w-full h-full">
+            <img
+                class="w-full h-full"
+                alt={file.name}
+                src={URL.createObjectURL(file)}
+            />
+        </div>
+    {:else if file.type.startsWith("audio/")}
+        <div class="flex items-center justify-center w-full h-full">
+            <Fa class="w-full h-full text-8xl" icon={faMusic} />
+        </div>
+    {:else if file.type.startsWith("video")}
+        <div class="flex items-center justify-center w-full h-full">
+            <Fa class="w-full h-full text-8xl" icon={faVideo} />
+        </div>
+    {:else}
+        <div class="flex items-center justify-center w-full h-full">
+            <Fa class="w-full h-full text-8xl" icon={faFile} />
+        </div>
+    {/if}
+</div>
---
+++ src/lib/components/message.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import { faDownload, faFile } from "@fortawesome/free-solid-svg-icons";
+    import type { Message } from "lib-concord-client";
+    import Fa from "svelte-fa";
+
+    const { url, message }: { url: string; message: Message } = $props();
+</script>
+
+<div class="bg-gray-800 p-2 rounded-md">
+    <p>{message.content}</p>
+    <div class="flex flex-col items-start gap-4">
+        {#each message.attachments as attachment}
+            <div
+                class="group flex relative p-2 bg-gray-900 rounded-2xl hover:rounded-tr-none max-h-96"
+            >
+                <a
+                    download={attachment.name}
+                    href={`${url}/files/${attachment.id}`}
+                    class="absolute hidden group-hover:flex -right-2 -top-2 bg-gray-900 p-1 rounded-sm cursor-pointer"
+                >
+                    <Fa class="text-2xl" icon={faDownload} />
+                </a>
+                {#if attachment.mime_type.startsWith("audio")}
+                    <audio controls src={`${url}/files/${attachment.id}`}
+                    ></audio>
+                {:else if attachment.mime_type.startsWith("image")}
+                    <img
+                        class="rounded-lg"
+                        alt={attachment.hash}
+                        src={`${url}/files/${attachment.id}`}
+                    />
+                {:else if attachment.mime_type.startsWith("video")}
+                    <video
+                        controls
+                        class="rounded-lg"
+                        src={`${url}/files/${attachment.id}`}
+                    >
+                        <track kind="captions" />
+                    </video>
+                {:else}
+                    <div>
+                        {attachment.name}
+                        <Fa class="text-5xl" icon={faFile} />
+                    </div>
+                {/if}
+            </div>
+        {/each}
+    </div>
+</div>
