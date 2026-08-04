8cdbfa26045fef93c25fd191587a82c999631f5a
Heráclito <heraclitoqsaldanha@gmail.com>
2026-08-04 09:26:03 -0300

    passa a usar pkdns para adicionar servidor

--- src/lib/components/addServerModal.svelte
+++ src/lib/components/addServerModal.svelte
@@ -1,24 +1,27 @@
 <script lang="ts">
-    import {
-        getInfo,
-        type GetInfoResponse,
-    } from "lib-concord-client/dist/http";
+    import { getInfo, type GetInfoResponse } from "lib-concord-client/dist/http";
+    import { faX } from "@fortawesome/free-solid-svg-icons";
+    import { DNSClient, ResolvePolicy, type HTTPSTypeRecordData } from "lib-concord-client/dist/pkdns";
     import Loading from "./loading.svelte";
     import Fa from "svelte-fa";
-    import { faX } from "@fortawesome/free-solid-svg-icons";
 
     let url = $state("");
+    let publicKey = $state("");
     let serverInfo: GetInfoResponse | undefined = $state();
 
     const { onServerAdd, onClose } = $props();
 
-    async function updateInfo(url: string) {
+    async function updateInfo(publicKey: string) {
         try {
-            if (URL.canParse(url)) {
-                serverInfo = await getInfo(url);
-            } else {
-                serverInfo = undefined;
-            }
+            const dnsClient = new DNSClient();
+            const entries = await dnsClient.resolve(publicKey, ResolvePolicy.NetworkOnly);
+            const entry = entries?.find((entry) => entry.rdata.type == "HTTPS");
+            const record = entry?.rdata as HTTPSTypeRecordData | undefined;
+
+            if (!record) return;
+
+            url = `https://${record.target}`;
+            serverInfo = await getInfo(url);
         } catch {
             serverInfo = undefined;
         }
@@ -32,13 +35,11 @@
     }
 
     $effect(() => {
-        updateInfo(url ?? "");
+        updateInfo(publicKey ?? "");
     });
 </script>
 
-<div
-    class="fixed top-0 left-0 backdrop-blur-xs z-10 flex flex-col w-screen h-screen items-center justify-center"
->
+<div class="fixed top-0 left-0 backdrop-blur-xs z-10 flex flex-col w-screen h-screen items-center justify-center">
     <form
         class="relative flex flex-col items-center bg-gray-900 gap-2 p-4 border border-gray-800"
         onsubmit={(event) => {
@@ -47,11 +48,7 @@
         }}
     >
         <div class="absolute top-3 right-3 flex w-full flex-row justify-end">
-            <button
-                class="flex hover:bg-white/10 p-1 rounded-md w-6 h-6 items-center justify-center cursor-pointer"
-                type="button"
-                onclick={onClose}
-            >
+            <button class="flex hover:bg-white/10 p-1 rounded-md w-6 h-6 items-center justify-center cursor-pointer" type="button" onclick={onClose}>
                 <Fa icon={faX}></Fa>
             </button>
         </div>
@@ -63,16 +60,8 @@
         {:else}
             <Loading class="w-40 h-40" />
         {/if}
-        <input
-            bind:value={url}
-            class="text-white p-2 rounded-md border-2 border-gray-700 bg-gray-800"
-            placeholder="https://example.com"
-        />
-        <button
-            type="submit"
-            disabled={serverInfo == undefined}
-            class="w-full bg-gray-800 text-white p-2 rounded-md cursor-pointer"
-            >Adicionar</button
+        <input bind:value={publicKey} class="text-white p-2 rounded-md border-2 border-gray-700 bg-gray-800" placeholder="PublicKey" />
+        <button type="submit" disabled={serverInfo == undefined} class="w-full bg-gray-800 text-white p-2 rounded-md cursor-pointer">Adicionar</button
         >
     </form>
 </div>
